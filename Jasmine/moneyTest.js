c5e49bbb5a1f3d28f72fab26a1bc308ac092daaf
Thirumalaivasan-Nagarajan <thirumalaivasann.al24@bitsathy.ac.in>
2026-02-11 12:16:02 +0530

    Update moneyTest.js

--- Jasmine/moneyTest.js
+++ Jasmine/moneyTest.js
@@ -1,1 +1,20 @@
+import { priceFormatting } from "../scripts/utils/money.js";
 
+describe("Test suite:Format Currency!!!",()=>{
+    it("Convert cents into currency",()=>
+    {
+        expect(priceFormatting(2065)).toEqual("20.65");
+    });
+
+    it("Edge case 1",()=>
+    {
+        expect(priceFormatting(2000.5)).toEqual("20.01");
+    });
+
+    it("Checking with zero",()=>
+    {
+        expect(priceFormatting(0)).toEqual("0.00");
+    })
+
+
+});
